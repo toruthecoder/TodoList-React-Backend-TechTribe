e8f63c6b624769f26a4c6821ab6fbf534c5beaaf
toruthecoder <devishsage@gmail.com>
2026-01-29 16:07:23 +0500

    change

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017');
+        await mongoose.connect(process.env.DB_URL);
         console.log('DB connected')
     } catch (error) {
         console.error(`Error Connecting DB`, error)
