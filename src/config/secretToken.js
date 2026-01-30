ba2b9166e13e780e8c1da87e8221bac3083ff691
toruthecoder <devishsage@gmail.com>
2026-01-30 14:02:48 +0500

    push

--- src/config/secretToken.js
+++ src/config/secretToken.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 dotenv.config()
 
 const createSecretToken = (id) => {
-    return jwt.sign({ id }, process.env.TOKEN_KEY, { expiresIn: 3 * 24 * 60 * 60, });
+    return jwt.sign({ id }, process.env.TOKEN_KEY, { expiresIn: 40 * 24 * 60 * 60, });
 }
 
 export default createSecretToken
